0ef32d87008b8ebbfa1192213528b737a7129a50
Kaido Arb <kaido.arb@tktk.ee>
2023-12-14 15:45:53 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// Comment
+
+//rakendus küsib kasutaja nime
+//rakendus tervitab nimepidi
+
+Console.WriteLine("Enter you name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + " " + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
